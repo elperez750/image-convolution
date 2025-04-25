a0e8728127e555bd0a6ee982fbdae1fe83dfd9f9
elperez1725 <elperez750@gmail.com>
2025-04-24 18:33:37 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex text-blue-500 justify-center items-center h-screen w-full">
+      Image Convolution app 
+    </div>
+  )
+}
+
+export default App
